733208845b3d074ea1924bb1c34190dda0d409bf
liditsfcs <lidits.fcs@uner.edu.ar>
2025-09-15 11:23:51 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,13 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
+
 //import "./styles.css";
 
-createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-);
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter basename="/asistencia-qr1">
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
+)
